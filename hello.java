673c7c9f1c503f66c4e80ed02355a183bfa3c530
Asad Siddiqui <hiasad@gmail.com>
2022-11-02 12:48:25 -0400

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,12 @@
-class Simple{
-    public static void main(String args[]){
+/**
+ * This is a useful comment about the package.
+ */
+package com.asad.simple;
+
+class Simple {
+    Simple() {
+    }
+    public static void main(final String[] args) {
         System.out.println("Hello Java");
     }
-}  
+}
